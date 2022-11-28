c9bca1a13660f6a904f8c712d032c19ff02a130f
magdalenaraa <magdalenaradanty@gmail.com>
2022-11-28 16:08:45 +0700

    update

---
+++ script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 'auto',
+    spaceBetween: 25,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    }
+});
